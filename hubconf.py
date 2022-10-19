c8e676b6e0f70d43c3ce33843d4e6138c4106609
Hawa Singh <cs21m006@iittp.ac.in>
2022-10-19 10:07:32 +0530

    Update hubconf.py
    connewralnetwork

--- hubconf.py
+++ hubconf.py
@@ -5,6 +5,9 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, ToPILImage
 from PIL import Image
 import torch.nn.functional as F
+import torch.optim as optim
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def load_data():
 
@@ -24,7 +27,7 @@ def load_data():
         transform=ToTensor(),
     )
 
-training_data, test_data = load_data()
+
 
     
 def create_dataloaders(training_data, test_data, batch_size=64):
@@ -40,7 +43,39 @@ def create_dataloaders(training_data, test_data, batch_size=64):
         
     return train_dataloader, test_dataloader
 
+
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+print(net)
+
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+training_data, test_data = load_data()
+
 train_dataloader, test_dataloader = create_dataloaders(training_data, test_data)
-
-
-
