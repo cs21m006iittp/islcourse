f8bfdc203fab337089a7cecc6061557a518eb333
Hawa Singh <cs21m006@iittp.ac.in>
2022-10-19 10:29:26 +0530

    Update hubconf.py
    Complete

--- hubconf.py
+++ hubconf.py
@@ -43,6 +43,40 @@ def create_dataloaders(training_data, test_data, batch_size=64):
         
     return train_dataloader, test_dataloader
 
+class ModifiedDataset(Dataset):
+  def __init__(self,given_dataset,shrink_percent=10):
+    self.given_dataset = given_dataset
+    self.shrink_percent = shrink_percent
+    
+  def __len__(self):
+    return len(self.given_dataset)
+
+  def __getitem__(self,idx):
+    img, lab = self.given_dataset[idx]
+
+    # print (type(img))
+    # print (img.shape)
+
+    img2 = transform_tensor_to_pil(img.squeeze())
+
+    # print (img2.size)
+    
+    new_w = int(img2.size[0]*(1-self.shrink_percent/100.0))
+    new_h = int(img2.size[1]*(1-self.shrink_percent/100.0))
+
+    # print (new_w, new_h)
+
+    img3 = img2.resize((new_w,new_h))
+
+    # print (img3.size)
+
+    x = transform_pil_to_tensor(img3)
+
+    # print (x.shape)
+
+    return x,lab
+
+
 
 
 
@@ -79,13 +113,51 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 training_data, test_data = load_data()
 
 train_dataloader, test_dataloader = create_dataloaders(training_data, test_data)
+input, label = trainingdata
+
+def test_model(dataloader, net, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    net.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = net(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+ def train_model(dataloader, net, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    net.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = net(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            
+            
+            
 
 
 epochs = 10 
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, net, loss_fn, optimizer)
-    test(test_dataloader, net, loss_fn)
+    train_model(train_dataloader, net, loss_fn, optimizer)
+    test_model(test_dataloader, net, loss_fn)
 print("Done!")
 
 
